bfb943fcbfc12826c36f3dd4dd9c6f66d9b3f37c
Mark Karpeles <magicaltux@gmail.com>
2024-10-02 02:32:07 +0900

    modify stuff a bit

--- info.go
+++ info.go
@@ -10,24 +10,24 @@ import (
 	"golang.org/x/crypto/ripemd160"
 )
 
-type Info struct {
+type Out struct {
 	Name   string `json:"name"`   // p2sh, etc
 	Script string `json:"script"` // out script
 	raw    []byte
 }
 
-func makeInfo(name string, scriptPiece ...[]byte) *Info {
+func makeOut(name string, scriptPiece ...[]byte) *Out {
 	v := slices.Concat(scriptPiece...)
-	return &Info{
+	return &Out{
 		Name:   name,
 		Script: hex.EncodeToString(v),
 		raw:    v,
 	}
 }
 
-// GetOutputs returns the potential outputs that can be opened in theory with the given pubkey. p2w* values are "pay to segwit" and
+// GetOuts returns the potential outputs that can be opened in theory with the given pubkey. p2w* values are "pay to segwit" and
 // can only be used on segwit-enabled chains.
-func GetOutputs(pubkey *secp256k1.PublicKey) []*Info {
+func GetOuts(pubkey *secp256k1.PublicKey) []*Out {
 	pubKeyComp := pubkey.SerializeCompressed()
 	pubKeyUncomp := pubkey.SerializeUncompressed()
 	pubKeyCompHash := cryptutil.Hash(pubKeyComp, sha256.New, ripemd160.New)
@@ -35,17 +35,17 @@ func GetOutputs(pubkey *secp256k1.PublicKey) []*Info {
 
 	// https://learnmeabitcoin.com/technical/script/
 
-	outScripts := []*Info{
-		makeInfo("p2pkh", []byte{0x76, 0xa9}, pushBytes(pubKeyCompHash), []byte{0x88, 0xac}),
-		makeInfo("p2pukh", []byte{0x76, 0xa9}, pushBytes(pubKeyUncompHash), []byte{0x88, 0xac}),
-		makeInfo("p2pk", pushBytes(pubKeyComp), []byte{0xac}),
-		makeInfo("p2puk", pushBytes(pubKeyUncomp), []byte{0xac}),
-		makeInfo("p2wpkh", []byte{0}, pushBytes(pubKeyCompHash)),
+	outScripts := []*Out{
+		makeOut("p2pkh", []byte{0x76, 0xa9}, pushBytes(pubKeyCompHash), []byte{0x88, 0xac}),
+		makeOut("p2pukh", []byte{0x76, 0xa9}, pushBytes(pubKeyUncompHash), []byte{0x88, 0xac}),
+		makeOut("p2pk", pushBytes(pubKeyComp), []byte{0xac}),
+		makeOut("p2puk", pushBytes(pubKeyUncomp), []byte{0xac}),
+		makeOut("p2wpkh", []byte{0}, pushBytes(pubKeyCompHash)),
 	}
 
 	for _, s := range outScripts {
-		outScripts = append(outScripts, makeInfo("p2sh:"+s.Name, []byte{0xa9}, pushBytes(cryptutil.Hash(s.raw, sha256.New, ripemd160.New)), []byte{0x87}))
-		outScripts = append(outScripts, makeInfo("p2wsh:"+s.Name, []byte{0}, pushBytes(cryptutil.Hash(s.raw, sha256.New))))
+		outScripts = append(outScripts, makeOut("p2sh:"+s.Name, []byte{0xa9}, pushBytes(cryptutil.Hash(s.raw, sha256.New, ripemd160.New)), []byte{0x87}))
+		outScripts = append(outScripts, makeOut("p2wsh:"+s.Name, []byte{0}, pushBytes(cryptutil.Hash(s.raw, sha256.New))))
 	}
 
 	return outScripts
