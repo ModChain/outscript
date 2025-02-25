5ae693e98b5579089c27187d7d3d6a738b954736
Mark Karpeles <magicaltux@gmail.com>
2025-02-25 16:48:50 +0900

    (small test)

---
+++ btcguess_test.go
@@ -0,0 +1,17 @@
+package outscript_test
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/ModChain/outscript"
+)
+
+func TestGuessOut(t *testing.T) {
+	// 76a9149e8985f82bc4e0f753d0492aa8d11cc39925774088ac
+	a, b := outscript.GuessPubKeyAndHashByOutScript(must(hex.DecodeString("76a9149e8985f82bc4e0f753d0492aa8d11cc39925774088ac")))
+
+	if a != nil || hex.EncodeToString(b) != "9e8985f82bc4e0f753d0492aa8d11cc399257740" {
+		t.Errorf("invalid result for GuessPubKeyAndHashByOutScript")
+	}
+}
